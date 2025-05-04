4aa44e67b3a63a7c755c287ac9c0dad56b5b95d6
Md Jahidul Islam <jahidcse181@gmail.com>
2025-05-04 20:27:30 +0600

    congiquration modified

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,9 @@ use App\Http\Middleware\Cors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -21,11 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->group('api', [
-            Cors::class, 
+            Cors::class,
             EnsureFrontendRequestsAreStateful::class, // ✅ Required for Sanctum authentication
             ThrottleRequests::class . ':api',
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
