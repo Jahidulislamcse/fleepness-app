ee9f35120adfef2403bbbd9b5b4623f78bcec415
Md Jahidul Islam <jahidcse181@gmail.com>
2025-08-03 12:41:20 +0600

    image path modified with app url

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -8,9 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
+
+    protected $appends = ['full_path'];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function getFullPathAttribute()
+    {
+        return url($this->path); // Uses APP_URL automatically
     }
 }
