df15f3ea7860b2de4853ef29b1082e30f7cebe14
Md Jahidul Islam <jahidcse181@gmail.com>
2025-10-21 20:10:41 +0600

    migration changed

--- database/migrations/2025_10_15_122242_add_platform_fee_added_to_orders_table.php
+++ database/migrations/2025_10_15_122242_add_platform_fee_added_to_orders_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->boolean('platform_fee_added')->default(false)->after('balance');
+            $table->boolean('platform_fee_added')->default(false);
         });
     }
 
